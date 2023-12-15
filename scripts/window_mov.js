e7eb9ab570a6450302a7bd8a78101932dcc3fc55
Antoine <me@antoinebellanger.fr>
2023-12-15 15:59:30 +0100

    [ENHANCEMENT] Rework file hierarchy

---
+++ scripts/window_mov.js
@@ -0,0 +1,6 @@
+import { windrag } from "../lib/js/windrag.js"
+windrag.create('#server-status', '#server-status-bar');
+
+//windrag.create('#stuff-I-do', '#stuff-I-do-bar');
+//windrag.create('#mainWindow', '#mainWindow-bar');
+//windrag.create('#about', '#about-bar');
